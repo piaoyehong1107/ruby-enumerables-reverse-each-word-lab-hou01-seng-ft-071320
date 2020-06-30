b43ed3c1096ede6b968b812df90c01153744cd73
Yehong Piao <piaoyehong1107@gmail.com>
2020-06-30 22:15:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def  reverse_each_word (sentence)
   new_sentence=[ ]
   array = sentence.split
   array.each do |element|
-    new_sentence = element.reverse
+    new_sentence.push(element.reverse)
   end
   new_sentence.join(" ")
 end
