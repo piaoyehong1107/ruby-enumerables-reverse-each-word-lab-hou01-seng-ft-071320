1eefdc9447cc735718522ecd6080f0c0775786f4
Yehong Piao <piaoyehong1107@gmail.com>
2020-06-30 22:16:02 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+def  reverse_each_word (sentence)
+  new_sentence=[ ]
+  array = sentence.split
+  array.collect do |element|
+    new_sentence.push(element.reverse)
+  end
+  new_sentence.join(" ")
+end
